2c90c61bfbd173a51284f8e3e24027af26a7ab0d
Kevin Veerasammy <kevinveerasammy@gmail.com>
2026-01-21 04:39:05 +0000

    updating app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-from snowflake.snowpark import Session
+
 
 import requests
 
@@ -47,7 +47,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-#         st.stop()
+        st.stop()
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
